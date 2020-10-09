a0de7d4f60601467b1788d4f8ddec5c193bde28d
Alex Tsang <alextsang@live.com>
2020-10-10 01:40:35 +0900

    Documentation

--- validators/wish.js
+++ validators/wish.js
@@ -2,10 +2,21 @@ const Ajv = require('ajv');
 
 const newWishSchema = require('./new-wish-schema.json');
 
+/**
+ * @typedef {import('../type-def').Wish} Wish
+ */
+
 const ajv = new Ajv();
 
 const newWishValidation = ajv.compile(newWishSchema);
 
+/**
+ * Checks if the wish is valid.
+ *
+ * @param {Wish} wish The wish from the user.
+ *
+ * @returns {boolean} True if the wish is valid, false otherwise.
+ */
 const isValidNewWish = (wish) => newWishValidation(wish);
 
 module.exports = {
